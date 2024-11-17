f572299f33845ed8049486d62f19e325e773e311
Asif-102 <asif1zero2@gmail.com>
2024-11-18 00:38:54 +0600

    post header body and action implemented done

---
+++ src/components/posts/PostList.jsx
@@ -0,0 +1,9 @@
+import PostCard from "./PostCard";
+
+export default function PostList({ posts }) {
+  return (
+    <>
+      {!!posts && posts.map((post) => <PostCard key={post.id} post={post} />)}
+    </>
+  );
+}
